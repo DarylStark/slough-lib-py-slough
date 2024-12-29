aa225ae9d6d1ccebaaa15bcb32121091e4e58e9a
Daryl Stark <daryl@dstark.nl>
2024-12-29 20:20:13 +0000

    style: Fix code style issues for linting

--- src/slough_cli_tool/__init__.py
+++ src/slough_cli_tool/__init__.py
@@ -1,3 +1,5 @@
+"""This module is the entry point for the CLI tool."""
+
 from .main import main
 
 __all__ = ['main']
--- src/slough_cli_tool/main.py
+++ src/slough_cli_tool/main.py
@@ -1,2 +1,6 @@
+"""Main module for slough-cli-tool."""
+
+
 def main() -> None:
+    """Entry point for the slough-cli-tool."""
     print('Hello from slough-cli-tool!')
