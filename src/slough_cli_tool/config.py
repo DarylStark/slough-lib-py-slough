41cc12b215ffe54a62c0c3ad31db22b20b6035c3
Daryl Stark <daryl@dstark.nl>
2025-01-07 19:59:37 +0000

    feat: Add project initialization command to CLI.
    Fixes #4

--- src/slough_cli_tool/config.py
+++ src/slough_cli_tool/config.py
@@ -3,7 +3,6 @@
 import typer
 
 from slough import Slough
-from slough_config import Author, ProjectInformation, SloughConfig
 
 from .generic import raise_for_missing_config
 from .output_formatters import OutputFormatter, OutputType
@@ -36,57 +35,3 @@ def cli_config_show(
         console.print(formatter.format(), end='')
     else:
         raise TypeError(f'Output type {output} not supported.')
-
-
-@config.command(name='init')
-def cli_config_init(
-    ctx: typer.Context,
-    title: str = typer.Option(
-        ...,
-        prompt='📛 Please enter the project title',
-        help='The project title',
-    ),
-    version: str = typer.Option(
-        ...,
-        prompt='🏷️  Please enter the project version',
-        help='The project version',
-    ),
-    author_name: str = typer.Option(
-        ...,
-        prompt='👤 Please enter the name of the author',
-        help='The project author name',
-    ),
-    author_email: str = typer.Option(
-        ...,
-        prompt='📧 Please enter the email of the author',
-        help='The project author email',
-    ),
-) -> None:
-    """Initialize a configuration file.
-
-    Args:
-        ctx (typer.Context): Typer context.
-        title (str): The project title.
-        version (str): The project version.
-        author_name (str): The author name.
-        author_email (str): The author
-    """
-    context = ctx.obj
-    slough: Slough = context['slough']
-
-    # Create a new Slough object
-    slough_config = SloughConfig(
-        project=ProjectInformation(
-            name=title,
-            version=version,
-            authors=[Author(name=author_name, email=author_email)],
-        )
-    )
-
-    # Set the configuration in the `Slough` object
-    # TODO: Make sure it is only done when there is no configurationfile
-    #       yet.
-    slough.config = slough_config
-
-    # Save the configuration
-    slough.save()
