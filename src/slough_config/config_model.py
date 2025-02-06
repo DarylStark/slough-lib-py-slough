a6c98b3c348b1601cd91f7e4052ebea0779172ce
Daryl Stark <daryl@dstark.nl>
2025-02-06 20:24:37 +0000

    feat: Enhance docstrings for configuration models.

--- src/slough_config/config_model.py
+++ src/slough_config/config_model.py
@@ -6,14 +6,27 @@ from pydantic import BaseModel, Field
 
 
 class Author(BaseModel):
-    """Model for the author information."""
+    """Model for the author information.
+
+    Attributes:
+        name (str): The name of the author.
+        email (str): The email address of the author. Must match the specified
+            pattern.
+    """
 
     name: str
     email: str = Field(pattern=r'^\S+@\S+\.\S+$')
 
 
 class ProjectInformation(BaseModel):
-    """Model for the project information."""
+    """Model for the project information.
+
+    Attributes:
+        name (str): The name of the project.
+        version (str): The version of the project. Must follow semantic
+            versioning.
+        authors (list[Author]): A list of authors involved in the project.
+    """
 
     name: str
     version: str = Field(pattern=r'^(\d+)\.(\d+)\.(\d+)(?:-\S+(?:\.(\d+))?)?$')
@@ -21,14 +34,27 @@ class ProjectInformation(BaseModel):
 
 
 class DevelopmentEnvironment(str, Enum):
-    """Enum for the development environment."""
+    """Enum for the development environment.
+
+    Attributes:
+        PYTHON_GENERIC (str): Represents a generic Python development
+            environment.
+        NODEJS_GENERIC (str): Represents a generic Node.js development
+            environment.
+    """
 
     PYTHON_GENERIC = 'python-generic'
     NODEJS_GENERIC = 'nodejs-generic'
 
 
 class SloughConfig(BaseModel):
-    """Model for the configuration file."""
+    """Model for the configuration file.
+
+    Attributes:
+        project (ProjectInformation): The project information.
+        development_environment (DevelopmentEnvironment | None): The
+            development environment, if specified.
+    """
 
     project: ProjectInformation
     development_environment: DevelopmentEnvironment | None = None
