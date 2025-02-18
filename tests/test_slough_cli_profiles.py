27cee8740f18b0cab37ec49d805f9da743e1e11d
Daryl Stark <daryl@dstark.nl>
2025-02-18 20:13:19 +0000

    test: Add tests for edge cases.

--- tests/test_slough_cli_profiles.py
+++ tests/test_slough_cli_profiles.py
@@ -18,6 +18,8 @@ from slough_cli_tool import app
         'acceptance',
         'development',
         'production',
+        'my-profile',
+        'another_profile-with-dashes',
     ],
 )
 def test_slough_cli_profiles_add(
@@ -50,6 +52,68 @@ def test_slough_cli_profiles_add(
     cli_runner.invoke(app, ['profiles', 'add', profile_name])
     result = cli_runner.invoke(app, ['profiles', 'list'])
     assert profile_name in result.stdout
+
+
+@pytest.mark.parametrize(
+    'profile_name',
+    ['production', 'acceptance', 'development', '_all', '_default'],
+)
+def test_slough_cli_profiles_add_existing_profile(
+    monkeypatch: pytest.MonkeyPatch,
+    cli_runner: CliRunner,
+    profile_name: str,
+) -> None:
+    """Test the `profiles add` command with a profile that already exists.
+
+    Args:
+        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
+        cli_runner (CliRunner): Typer CLI runner.
+        profile_name (str): Profile name.
+    """
+    monkeypatch.chdir('tests/test_data/project7/')
+    result = cli_runner.invoke(app, ['profiles', 'add', profile_name])
+    assert result.exit_code == 1
+    assert type(result.exception) is ValueError
+
+
+@pytest.mark.parametrize(
+    'profile_name',
+    ['_all', '_default', '1test', 'with spaces', 'with.dots', 'with,commas'],
+)
+def test_slough_cli_profiles_add_invalid_profile(
+    empty_test_dir: Path,
+    cli_runner: CliRunner,
+    profile_name: str,
+) -> None:
+    """Test the `profiles add` command with a invalid profile name.
+
+    Args:
+        empty_test_dir (Path): Empty test directory.
+        cli_runner (CliRunner): Typer CLI runner.
+        profile_name (str): Profile name.
+    """
+    cli_runner.invoke(
+        app,
+        [
+            '--cfgfile',
+            'slough.yml',
+            'project',
+            'init',
+            '--title',
+            'test_project',
+            '--version',
+            '1.0.0',
+            '--author-name',
+            'Daryl Stark',
+            '--author-email',
+            'daryl@example.com',
+        ],
+    )
+    result = cli_runner.invoke(
+        app, ['--cfgfile', 'slough.yml', 'profiles', 'add', profile_name]
+    )
+    assert result.exit_code == 1
+    assert type(result.exception) is ValueError
 
 
 def test_slough_cli_profiles_list(
@@ -125,6 +189,46 @@ def test_slough_cli_profiles_remove(
 
 
 @pytest.mark.parametrize(
+    'profile_name',
+    ['_all', '_default'],
+)
+def test_slough_cli_profiles_remove_default_profile(
+    monkeypatch: pytest.MonkeyPatch, cli_runner: CliRunner, profile_name: str
+) -> None:
+    """Test the `profiles remove` command with the default profile.
+
+    Args:
+        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
+        cli_runner (CliRunner): Typer CLI runner.
+        profile_name (str): Profile name.
+    """
+    monkeypatch.chdir('tests/test_data/project7/')
+    result = cli_runner.invoke(app, ['profiles', 'remove', profile_name])
+    assert result.exit_code == 1
+    assert type(result.exception) is ValueError
+
+
+@pytest.mark.parametrize(
+    'profile_name',
+    ['non-existing-profile', 'my_profile', 'another_profile'],
+)
+def test_slough_cli_profiles_remove_non_existing_profile(
+    monkeypatch: pytest.MonkeyPatch, cli_runner: CliRunner, profile_name: str
+) -> None:
+    """Test the `profiles remove` command with a non-existing profile.
+
+    Args:
+        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
+        cli_runner (CliRunner): Typer CLI runner.
+        profile_name (str): Profile name.
+    """
+    monkeypatch.chdir('tests/test_data/project7/')
+    result = cli_runner.invoke(app, ['profiles', 'remove', profile_name])
+    assert result.exit_code == 1
+    assert type(result.exception) is ValueError
+
+
+@pytest.mark.parametrize(
     'profile_name, new_name',
     [
         ('production', 'test_name'),
@@ -152,3 +256,35 @@ def test_slough_cli_profiles_rename(
     assert new_name in result.stdout
 
     cli_runner.invoke(app, ['profiles', 'rename', new_name, profile_name])
+
+
+@pytest.mark.parametrize(
+    'profile_name, new_name',
+    [
+        ('production', 'acceptance'),
+        ('acceptance', 'development'),
+        ('development', 'production'),
+    ],
+)
+def test_slough_cli_profiles_rename_to_existing(
+    monkeypatch: pytest.MonkeyPatch,
+    cli_runner: CliRunner,
+    profile_name: str,
+    new_name: str,
+) -> None:
+    """Test the `profiles rename` command.
+
+    Ranames the profile to a existing profile name. Should fail.
+
+    Args:
+        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
+        cli_runner (CliRunner): Typer CLI runner.
+        profile_name (str): Profile name.
+        new_name (str): New profile name.
+    """
+    monkeypatch.chdir('tests/test_data/project7/')
+    result = cli_runner.invoke(
+        app, ['profiles', 'rename', profile_name, new_name]
+    )
+    assert result.exit_code == 1
+    assert type(result.exception) is ValueError
