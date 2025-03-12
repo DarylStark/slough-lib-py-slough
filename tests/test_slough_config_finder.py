275ceed68e0d1551d1da25a5cccbb2e0bcdce0aa
Daryl Stark <daryl@dstark.nl>
2025-03-12 20:35:49 +0000

    test: Add tests for ConfigFileFinder in subdir.

--- tests/test_slough_config_finder.py
+++ tests/test_slough_config_finder.py
@@ -1,10 +1,8 @@
 """Tests for the ConfigFileFinder."""
 
-import random
-import string
 import tempfile
+from collections.abc import Generator
 from pathlib import Path
-from typing import Generator
 
 import pytest
 
@@ -39,6 +37,36 @@ def test_config_file_finder_existing_file_root(temp_folder: Path) -> None:
     assert (
         ConfigFileFinder(
             'slough.yml', working_dir=temp_folder, max_directory_depth=1
+        ).find()
+        == config_file.resolve()
+    )
+
+
+def test_config_file_finder_existing_file_subdir(temp_folder: Path) -> None:
+    """Test the ConfigFileFinder when cwd is in a subdirectory."""
+    subdir = temp_folder / 'subdir'
+    subdir.mkdir()
+    config_file = temp_folder / 'slough.yml'
+    config_file.touch()
+    assert (
+        ConfigFileFinder(
+            'slough.yml', working_dir=subdir, max_directory_depth=2
+        ).find()
+        == config_file.resolve()
+    )
+
+
+def test_config_file_finder_existing_file_three_deep_subdir(
+    temp_folder: Path,
+) -> None:
+    """Test the ConfigFileFinder when cwd is in a three deep subdirectory."""
+    subdir = temp_folder / 'subdir' / 'subdir' / 'subdir'
+    subdir.mkdir(parents=True)
+    config_file = temp_folder / 'slough.yml'
+    config_file.touch()
+    assert (
+        ConfigFileFinder(
+            'slough.yml', working_dir=subdir, max_directory_depth=4
         ).find()
         == config_file.resolve()
     )
