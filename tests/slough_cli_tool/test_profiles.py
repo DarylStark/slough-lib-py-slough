4a05a5eba19326cfcb86583e5d0c062c6e6d53fd
Daryl Stark <daryl@dstark.nl>
2025-03-27 20:13:23 +0000

    test: Add tests for profile management commands.

--- tests/slough_cli_tool/test_profiles.py
+++ tests/slough_cli_tool/test_profiles.py
@@ -23,20 +23,18 @@ from slough_cli_tool import app
     ],
 )
 def test_slough_cli_profiles_add(
-    cli_runner: CliRunner, empty_test_dir: Path, profile_name: str
+    cli_runner: CliRunner, temp_folder: Path, profile_name: str
 ) -> None:
     """Test the `profiles add` command.
 
     Args:
         cli_runner (CliRunner): Typer CLI runner.
-        empty_test_dir (Path): Empty test directory.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
     cli_runner.invoke(
         app,
         [
-            '--cfgfile',
-            'slough.yml',
             'project',
             'init',
             '--title',
@@ -49,28 +47,29 @@ def test_slough_cli_profiles_add(
             'daryl@example.com',
         ],
     )
-    cli_runner.invoke(app, ['profiles', 'add', profile_name])
-    result = cli_runner.invoke(app, ['profiles', 'list'])
-    assert profile_name in result.stdout
+    result_add = cli_runner.invoke(app, ['profiles', 'add', profile_name])
+    assert result_add.exit_code == 0
+    result_list = cli_runner.invoke(app, ['profiles', 'list'])
+    assert profile_name in result_list.stdout
 
 
 @pytest.mark.parametrize(
     'profile_name',
-    ['production', 'acceptance', 'development', '_all', '_default'],
+    ['production', 'acceptance', 'development'],
 )
 def test_slough_cli_profiles_add_existing_profile(
-    monkeypatch: pytest.MonkeyPatch,
     cli_runner: CliRunner,
+    temp_folder: Path,
     profile_name: str,
 ) -> None:
     """Test the `profiles add` command with a profile that already exists.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
         cli_runner (CliRunner): Typer CLI runner.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
+    _ = cli_runner.invoke(app, ['profiles', 'add', profile_name])
     result = cli_runner.invoke(app, ['profiles', 'add', profile_name])
     assert result.exit_code == 1
     assert type(result.exception) is ValueError
@@ -81,52 +80,41 @@ def test_slough_cli_profiles_add_existing_profile(
     ['_all', '_default', '1test', 'with spaces', 'with.dots', 'with,commas'],
 )
 def test_slough_cli_profiles_add_invalid_profile(
-    empty_test_dir: Path,
     cli_runner: CliRunner,
+    temp_folder: Path,
     profile_name: str,
 ) -> None:
     """Test the `profiles add` command with a invalid profile name.
 
     Args:
-        empty_test_dir (Path): Empty test directory.
         cli_runner (CliRunner): Typer CLI runner.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
-    cli_runner.invoke(
-        app,
-        [
-            '--cfgfile',
-            'slough.yml',
-            'project',
-            'init',
-            '--title',
-            'test_project',
-            '--version',
-            '1.0.0',
-            '--author-name',
-            'Daryl Stark',
-            '--author-email',
-            'daryl@example.com',
-        ],
-    )
-    result = cli_runner.invoke(
-        app, ['--cfgfile', 'slough.yml', 'profiles', 'add', profile_name]
-    )
+    result = cli_runner.invoke(app, ['profiles', 'add', profile_name])
     assert result.exit_code == 1
     assert type(result.exception) is ValueError
 
 
 def test_slough_cli_profiles_list(
-    monkeypatch: pytest.MonkeyPatch, cli_runner: CliRunner
+    cli_runner: CliRunner,
+    temp_folder: Path,
 ) -> None:
     """Test the `profiles list` command.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
+        temp_folder (Path): Temporary folder for testing.
         cli_runner (CliRunner): Typer CLI runner.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
+    for profile_name in [
+        'production',
+        'acceptance',
+        'development',
+    ]:
+        cli_runner.invoke(app, ['profiles', 'add', profile_name])
     result = cli_runner.invoke(app, ['profiles', 'list'])
+    assert '_all' in result.stdout
+    assert '_default' in result.stdout
     assert 'production' in result.stdout
     assert 'acceptance' in result.stdout
     assert 'development' in result.stdout
@@ -146,44 +134,18 @@ def test_slough_cli_profiles_list(
 )
 def test_slough_cli_profiles_remove(
     cli_runner: CliRunner,
-    empty_test_dir: Path,
+    temp_folder: Path,
     profile_name: str,
 ) -> None:
     """Test the `profiles remove` command.
 
     Args:
         cli_runner (CliRunner): Typer CLI runner.
-        empty_test_dir (Path): Empty test directory.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
-    # Create a project
-    cli_runner.invoke(
-        app,
-        [
-            '--cfgfile',
-            'slough.yml',
-            'project',
-            'init',
-            '--title',
-            'test_project',
-            '--version',
-            '1.0.0',
-            '--author-name',
-            'Daryl Stark',
-            '--author-email',
-            'daryl@example.com',
-        ],
-    )
-
-    # Add a profile
     cli_runner.invoke(app, ['profiles', 'add', profile_name])
-    result = cli_runner.invoke(app, ['profiles', 'list'])
-    assert profile_name in result.stdout
-
-    # Remove the profile
     cli_runner.invoke(app, ['profiles', 'remove', profile_name])
-
-    # Check if the profile is removed
     result = cli_runner.invoke(app, ['profiles', 'list'])
     assert profile_name not in result.stdout
 
@@ -193,16 +155,15 @@ def test_slough_cli_profiles_remove(
     ['_all', '_default'],
 )
 def test_slough_cli_profiles_remove_default_profile(
-    monkeypatch: pytest.MonkeyPatch, cli_runner: CliRunner, profile_name: str
+    cli_runner: CliRunner, temp_folder: Path, profile_name: str
 ) -> None:
     """Test the `profiles remove` command with the default profile.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
         cli_runner (CliRunner): Typer CLI runner.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
     result = cli_runner.invoke(app, ['profiles', 'remove', profile_name])
     assert result.exit_code == 1
     assert type(result.exception) is ValueError
@@ -213,16 +174,15 @@ def test_slough_cli_profiles_remove_default_profile(
     ['non-existing-profile', 'my_profile', 'another_profile'],
 )
 def test_slough_cli_profiles_remove_non_existing_profile(
-    monkeypatch: pytest.MonkeyPatch, cli_runner: CliRunner, profile_name: str
+    cli_runner: CliRunner, temp_folder: Path, profile_name: str
 ) -> None:
     """Test the `profiles remove` command with a non-existing profile.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
         cli_runner (CliRunner): Typer CLI runner.
+        temp_folder (Path): Temporary folder for testing.
         profile_name (str): Profile name.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
     result = cli_runner.invoke(app, ['profiles', 'remove', profile_name])
     assert result.exit_code == 1
     assert type(result.exception) is ValueError
@@ -237,7 +197,6 @@ def test_slough_cli_profiles_remove_non_existing_profile(
     ],
 )
 def test_slough_cli_profiles_rename(
-    monkeypatch: pytest.MonkeyPatch,
     cli_runner: CliRunner,
     profile_name: str,
     new_name: str,
@@ -245,17 +204,14 @@ def test_slough_cli_profiles_rename(
     """Test the `profiles rename` command.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
         cli_runner (CliRunner): Typer CLI runner.
         profile_name (str): Profile name.
         new_name (str): New profile name.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
+    cli_runner.invoke(app, ['profiles', 'add', profile_name])
     cli_runner.invoke(app, ['profiles', 'rename', profile_name, new_name])
     result = cli_runner.invoke(app, ['profiles', 'list'])
     assert new_name in result.stdout
-
-    cli_runner.invoke(app, ['profiles', 'rename', new_name, profile_name])
 
 
 @pytest.mark.parametrize(
@@ -267,7 +223,6 @@ def test_slough_cli_profiles_rename(
     ],
 )
 def test_slough_cli_profiles_rename_to_existing(
-    monkeypatch: pytest.MonkeyPatch,
     cli_runner: CliRunner,
     profile_name: str,
     new_name: str,
@@ -277,12 +232,13 @@ def test_slough_cli_profiles_rename_to_existing(
     Ranames the profile to a existing profile name. Should fail.
 
     Args:
-        monkeypatch (pytest.MonkeyPatch): Pytest monkeypatch fixture.
         cli_runner (CliRunner): Typer CLI runner.
         profile_name (str): Profile name.
         new_name (str): New profile name.
     """
-    monkeypatch.chdir('tests/test_data/project7/')
+    cli_runner.invoke(app, ['profiles', 'add', profile_name])
+    cli_runner.invoke(app, ['profiles', 'add', new_name])
+
     result = cli_runner.invoke(
         app, ['profiles', 'rename', profile_name, new_name]
     )
